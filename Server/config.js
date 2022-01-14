be69529078155814e099f5e997ef2102cfefd896
YouYou Yu <youyy1225@gmail.com>
2022-01-14 12:49:34 -0600

    a

---
+++ Server/config.js
@@ -0,0 +1,9 @@
+
+let config = {
+    host    : '127.0.0.1',
+    user    :'root',
+    password:'290629',
+    database:'chuckit_test'
+};
+
+module.exports = config;
---
+++ Server/insert.js
@@ -0,0 +1,19 @@
+let mysql = require('mysql2');
+let config = require('./config.js');
+let connection = mysql.createConnection(config);
+
+connection.connect(function(err){
+    if(err){
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+});
+
+
+//insert SQL statements
+let insertwebsite = `   INSERT INTO websites(url)
+                        VALUES('www.google.com');       `;
+
+connection.query(insertwebsite);
+
+connection.end();
